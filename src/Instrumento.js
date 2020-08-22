ba036c4f74b2d92c813c83bd6739c66b83a4122d
Cole Gawin <colegawin@gmail.com>
2020-08-22 15:22:54 -0500

    Fix image switching issues.

--- src/Instrumento.js
+++ src/Instrumento.js
@@ -4,11 +4,12 @@ import './instrumento.css';
 class Instrumento extends React.Component {
 
     //Ok, nesse onDragStart parece que salvamos as informações necessárias para depois
-    onDragStart = (e, posicao, nome, funcao, resposta) => {
+    onDragStart = (e, posicao, nome, funcao, resposta, src) => {
         e.dataTransfer.setData("posicao", posicao);
         e.dataTransfer.setData("nome", nome);
         e.dataTransfer.setData("funcao", funcao);
         e.dataTransfer.setData("resposta", resposta);
+        e.dataTransfer.setData("src", src);
     }
 
     render(){
@@ -20,6 +21,7 @@ class Instrumento extends React.Component {
                 this.props.inst.nome,
                 this.props.inst.função,
                 this.props.inst.resposta,
+              this.props.inst.src
             )}>
                 <p className="Instrumento">{this.props.inst.nome}</p>
                 <img alt="Imagem Instrumento" className="imagemInstrumento" src={this.props.inst.src}></img>
